c5ac44d7dbca0433efa930577fccd1dc668a8f7e
xusira <xusira@outlook.com>
2017-10-09 07:04:07 -0500

    Re-added project files. Eclipse project should not be included. Only classes should be tracked.

---
+++ src/DodgeCharger.java
@@ -0,0 +1,19 @@
+
+public class DodgeCharger extends Vehicle
+{
+	@Override
+	public void name()
+	{
+		System.out.println("dodge charger");
+	}
+
+	@Override
+	public void engineSound()
+	{
+	}
+	
+	public void touchscreen()
+	{
+		System.out.println("boop");
+	}
+}
---
+++ src/Lamborghini.java
@@ -0,0 +1,18 @@
+
+public class Lamborghini extends Vehicle
+{
+	@Override
+	public void engineSound()
+	{
+		System.out.println("i'm a v12"); 
+	}
+	@Override
+	public void name()
+	{
+		System.out.println("Galardo");
+	}
+	public void quadExhaust()
+	{
+		System.out.println("pollution");
+	}
+}
