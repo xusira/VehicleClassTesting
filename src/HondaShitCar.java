26329d1707cfe512a36bfc711544286ee4fc0fde
xusira <xusira@outlook.com>
2017-10-09 07:23:44 -0500

    Re-added HondaShitCar manually.

---
+++ src/HondaShitCar.java
@@ -0,0 +1,15 @@
+
+public class HondaShitCar extends Vehicle
+{
+	@Override
+	public void name()
+	{
+		System.out.println("Honda");
+	}
+
+	@Override
+	public void engineSound()
+	{
+		System.out.println("clank clank clank");
+	}
+}
